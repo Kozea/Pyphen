a15ec969f6e89b70f0de7bb125ee402224d2aa4a
wbsoft <wbsoft@9ccaac5f-f547-0410-a4bc-9f33b0f64cd5>
2008-03-08 23:36:02 +0000

    Import. For history of hyphenator.py, see the LilyKDE project at http://lilykde.googlecode.com/.

---
+++ setup.py
@@ -0,0 +1,67 @@
+from distutils.core import setup
+
+longdesc = """
+Python text hyphenator
+======================
+
+hyphenator is a pure Python module to hyphenate text using existing hyphenation
+dictionaries, like those used by OpenOffice.org.
+
+Usage:
+
+>>> from hyphenator import Hyphenator
+>>> h = Hyphenator("/usr/share/myspell/hyph_nl_NL.dic")
+>>> h.inserted('lettergrepen')
+u'let-ter-gre-pen'
+>>> h.wrap('autobandventieldopje', 11)
+('autoband-', 'ventieldopje')
+>>> for pair in h.iterate('Amsterdam'):
+...     print pair
+...
+('Amster', 'dam')
+('Am', 'sterdam')
+>>>
+
+Features:
+
+* 100% pure Python
+* caches dict files and hyphenated words
+* supports nonstandard hyphenation patterns
+
+License:
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+"""
+
+classifiers = [
+'Development Status :: 3 - Alpha',
+'Intended Audience :: Developers',
+'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+'Programming Language :: Python',
+'Topic :: Text Processing',
+'Topic :: Text Processing :: Linguistic',
+]
+
+setup(
+    name = 'hyphenator',
+    version = '0.5',
+    py_modules = ['hyphenator'],
+    author = 'Wilbert Berendsen',
+    author_email = 'wbsoft@xs4all.nl',
+    url = 'http://hyphenator.googlecode.com/',
+    description = 'Pure Python module to hyphenate text using existing dictionaries',
+    long_description = longdesc,
+    classifiers = classifiers,
+)
