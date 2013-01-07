07a5906f304fe2ea5bd1648a3cb96837f4991601
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2013-01-07 18:52:01 +0100

    Overwrite dictionaries on update

--- dictionaries/update.sh
+++ dictionaries/update.sh
@@ -2,6 +2,6 @@ host='http://cgit.freedesktop.org/'
 
 for folder in `curl -s $host/libreoffice/dictionaries/tree/ | grep 'ls-dir' | cut -d "'" -f 6`; do
     for file in `curl -s $host$folder | grep 'ls-blob' | grep 'hyph_.*\.dic' | cut -d "'" -f 6`; do
-        wget `echo $host$file | sed 's/tree/plain/'`
+        wget -N `echo $host$file | sed 's/tree/plain/'`
     done
 done
