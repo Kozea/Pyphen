85c12c1a188ec4b58d407e188857e94a0446925f
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2013-08-09 17:47:38 +0200

    Version 0.8

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ classifiers = [
 _dict_folder = os.path.join(os.path.dirname(__file__), 'dictionaries')
 setup(
     name='Pyphen',
-    version='0.7',
+    version='0.8',
     py_modules=['pyphen'],
     provides=['pyphen'],
     data_files=[(
