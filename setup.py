f002480719d2d31466324f0520777090470f2ce4
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2018-08-24 11:15:46 +0200

    Update Bulgarian and Spanish dictionaries, add Indonesian
    Version 0.9.5.

--- setup.py
+++ setup.py
@@ -11,14 +11,12 @@ classifiers = [
     'License :: OSI Approved :: Mozilla Public License 1.1 (MPL 1.1)',
     'Programming Language :: Python',
     'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.6',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.1',
-    'Programming Language :: Python :: 3.2',
-    'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: Implementation :: CPython',
     'Programming Language :: Python :: Implementation :: PyPy',
     'Topic :: Text Processing',
@@ -27,7 +25,7 @@ classifiers = [
 
 setup(
     name='Pyphen',
-    version='0.9.4',
+    version='0.9.5',
     provides=['pyphen'],
     packages=['pyphen'],
     package_data={'pyphen': [os.path.join(
