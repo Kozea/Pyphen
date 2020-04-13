287ac544bb9ab08a098b09325324f15d745f5064
Guillaume Ayoub <guillaume+github@yabz.fr>
2020-04-13 14:25:56 +0200

    Improve dictionaries update script

--- pyphen/dictionaries/update.sh
+++ pyphen/dictionaries/update.sh
@@ -2,6 +2,9 @@ host='https://cgit.freedesktop.org/'
 
 for folder in `curl -s $host/libreoffice/dictionaries/tree/ | grep 'ls-dir' | cut -d "'" -f 6`; do
     for file in `curl -s $host$folder | grep 'ls-blob' | grep 'hyph_.*\.dic' | cut -d "'" -f 6`; do
-        wget -N `echo $host$file | sed 's/tree/plain/'`
+        wget -N `echo $host$file | sed 's/tree/plain/'` &
     done
 done
+
+rename -- -Latn _Latn *-Latn.dic
+rename _ANY "" *_ANY.dic
